4ca23969b4d75289cf976b63e3fe793dccc464fd
Eric Solomon <esculpt@gmail.com>
2023-01-08 17:09:09 -0500

    refactoring code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,32 +1,22 @@
-const { get } = require('request');
+const { doesNotMatch } = require('assert');
 const request = require('request');
+const {get} = require('request');
 //check body type
-let data ;
-const arg = process.argv[2];
+let data;
 
-request("https://api.thecatapi.com/v1/breeds/search?q="+arg, function (error, response, body) {
-  //console.log(typeof body);
-  console.log(body);
-  
-  data = JSON.parse(body);
+const fetchBreedDescription =  function (breedName, callback) {
+  request("https://api.thecatapi.com/v1/breeds/search?q=" + breedName, function (error, response, body) {
+    //console.log(typeof body);
+    data = JSON.parse(body);
+    if (breedName.toLowerCase() !== data[0].name.toLowerCase()) {
+       return error;
+    }
+    if (response === 403 || response === 404) {
+      return error;
+    }
+    return data[0];
+    done();
+  })
+};
 
-  if (arg.toLowerCase() !== data[0].name.toLowerCase()) {
-    console.error("breed not found");
-    return;
-  }  
-  if (response === 403 || respone === 404) {
-    console.error("request failed");
-  }
-
-
-  
-  console.log(data[0].description);
-
-})
-
-
-
-
-
-
-
+module.exports = {fetchBreedDescription};
